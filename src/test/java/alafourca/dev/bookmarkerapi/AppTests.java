7df1c7436ae9027235f705b5f3147769e409412c
Ale <alafourcadedespaigne@gmail.com>
2023-12-22 16:19:40 -0300

    Project configuration

---
+++ src/test/java/alafourca/dev/bookmarkerapi/AppTests.java
@@ -0,0 +1,13 @@
+package alafourca.dev.bookmarkerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
